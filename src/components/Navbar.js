a21cb45636b9129d74fee67e85da159ccf28bc9c
0rGaan1c <nathan4drake1999@gmail.com>
2021-05-15 15:49:33 +0530

    some minor changes to ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,10 @@ const Navbar = () => {
       <nav className="bg-black">
         <div className="flex justify-between items-center text-white text-2xl p-3 font-bold tracking-wide cursor-pointer md:text-3xl md:py-5 md:w-11/12 md:mx-auto">
           <Link to="/">Cloud</Link>
-          <FaShoppingCart />
+          <div className="flex">
+            <FaShoppingCart className="transition duration-200 ease-in-out transform hover:scale-110" />
+            <div className="text-sm relative bottom-2 ml-1">0</div>
+          </div>
         </div>
       </nav>
     </>
